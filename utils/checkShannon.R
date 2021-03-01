9c61dd69b2f2c0c62dec2d2c97ecc28b44f557ef
ggnatalia <ngarcia@cnb.csic.es>
2021-03-01 19:54:05 +0100

    check

--- utils/checkShannon.R
+++ utils/checkShannon.R
@@ -8,12 +8,11 @@ library(vegan)
 library(reshape2)
 library(dplyr)
 
-Hvalues = seq(1, 10, 2) #x # Shannon 0 makes no sense => 1 species
-Svalues = seq(100, 5000, 500)  #y                 
+Hvalues = seq(1, 10, 0.1) #x # Shannon 0 makes no sense => 1 species
+Svalues = seq(100, 5000, 100)  #y                 
 reads = c(1000, 10000, 100000, 1000000)
-reads = 10000
 
-summary_fun = function(H, S, reads, meanlog = 0, replicas = 5) {
+summary_fun = function(H, S, reads, meanlog = 0, replicas = 100) {
     if ((log(S) - H) < 0 ){
         print(S)
         print(H)
@@ -39,9 +38,10 @@ summary_fun = function(H, S, reads, meanlog = 0, replicas = 5) {
             }
         }
         mean_H = mean(apply(df, 2, function(x) diversity(x, index = 'shannon')))
-        sd_H = mean(apply(df, 2, function(x) diversity(x, index = 'shannon')))
+        sd_H = sd(apply(df, 2, function(x) diversity(x, index = 'shannon')))
         mean_S = apply(df, 2, function(x) sum(x > 0))
-        sd_S = apply(df, 2, function(x) sum(x > 0))
+        #sd_S = apply(df, 2, function(x) sum(x > 0)) # revisar
+        sd_S = NA
         return(c(mean_H, sd_H, mean_S, sd_S))
     }
 }
@@ -63,9 +63,6 @@ for (r in reads){
         gg$S_real_mean[i] = results[3]
         gg$S_real_sd[i] = results[4]
     }
-    
-    gg$Herror = (gg$H-gg$H_real_mean)/gg$H
-    gg$Serror = (gg$S-gg$S_real_mean)/gg$S
     gg$reads = r
     data2plot[[as.character(r)]] = gg
     # SHANNON
@@ -78,78 +75,98 @@ for (r in reads){
     #p
 
     
-    d2plot_H = gg %>% select(H, S, Herror) %>% acast(H~S, value.var = 'Herror')
+    #d2plot_H = gg %>% select(H, S, Herror) %>% acast(H~S, value.var = 'Herror')
     #svg(paste0('Herror_', r,'.svg'), width = 10, height = 10, pointsize = 12)
-    filled.contour(d2plot_H,
-                   color.palette = colorRampPalette(c('firebrick1','tan1', 'lemonchiffon','tan1', 'firebrick1')),
-                   plot.title = title(main = 'Herror',  ylab = 'S', xlab = 'H'),
-                   plot.axes = {
-                       axis(1, at = seq(from=0, to = 1, length.out = nrow(d2plot_H)), labels = rownames(d2plot_H), las=2) #axis = 1 below
-                       axis(2, at = seq(from=0, to = 1, length.out = ncol(d2plot_H)), labels = colnames(d2plot_H), cex.axis=0.9, las=2) #axis = 2 left
-                       #axis(2, at =seq(from=0,to=1, length.out = 5), labels =c(4,3,2,1,0), cex.axis=0.9, las=2)
-                       
-                   }
-    )
-    #dev.off()
     
-    d2plot_S = gg %>% select(H, S, Serror) %>% acast(H~S, value.var = 'Serror', fill=0)
-    #svg(paste0('Serror_', r,'.svg'), width = 10, height = 10, pointsize = 12)
-    filled.contour(d2plot_S, 
-                   color.palette = colorRampPalette(c('firebrick1','tan1', 'lemonchiffon','tan1', 'firebrick1')),
-                   plot.title = title(main = 'Serror',  ylab = 'S', xlab = 'H'),
-                   plot.axes = {
-                       axis(1, at = seq(from=0, to = 1, length.out = nrow(d2plot_S)), labels = rownames(d2plot_S), las=2) #axis = 1 below
-                       axis(2, at = seq(from=0, to = 1, length.out = ncol(d2plot_S)), labels = colnames(d2plot_S), cex.axis=0.9, las=2) #axis = 2 left
-                   #axis(2, at =seq(from=0,to=1, length.out = 5), labels =c(4,3,2,1,0), cex.axis=0.9, las=2)
-                       
-                   }
-    )
-    #dev.off()
+    #Hggmelt = melt(gg, id.var = c('H','S'), measure.vars = c('Herror'))
+    #p = ggplot(Hggmelt, aes(H, S)) + theme_light()
+    #p = p + geom_raster(aes(fill = value), interpolate = T)
+    #p = p + labs(title = 'Shannon reference values', x = 'H', y = 'S', fill = 'H relative error')
+    #p = p + theme(plot.title = element_text(hjust = 0.5))
+    #p = p + scale_fill_gradient2(low = 'firebrick', mid = 'yellowgreen', high = 'firebrick', na.value = 'white', breaks = seq(0,1,0.1))
+    #p
+
+    #Sggmelt = melt(gg, id.var = c('H','S'), measure.vars = c('Serror'))
+    #p = ggplot(Sggmelt, aes(H, S)) + theme_light()
+    #p = p + geom_raster(aes(fill = value), interpolate = T)
+    #p = p + labs(title = 'Shannon reference values', x = 'H', y = 'S', fill = 'S relative error')
+    #p = p + theme(plot.title = element_text(hjust = 0.5))
+    #p = p + scale_fill_gradient2(low = 'firebrick', mid = 'yellowgreen', high = 'firebrick', na.value = 'white')
+    #p
+    
+    #ggmelt = melt(gg, id.var = c('H','S'), measure.vars = c('Herror', 'Serror'))
+    #p = ggplot(ggmelt, aes(H, S)) + theme_light() + facet_grid(.~variable)
+    #p = p + geom_raster(aes(fill = value), interpolate = T)
+    #p = p + labs(title = 'Shannon reference values', x = 'H', y = 'S', fill = 'S relative error')
+    #p = p + theme(plot.title = element_text(hjust = 0.5))
+    #p = p + scale_fill_gradient2(low = 'firebrick', mid = 'yellowgreen', high = 'firebrick', na.value = 'white')
+    #p
 }
 
 
+dd = do.call(rbind.data.frame, data2plot)
+dd$Herror = (dd$H-dd$H_real_mean)/dd$H
+dd$Serror = (dd$S-dd$S_real_mean)/dd$S
+ggmelt = melt(dd, id.var = c('H','S', 'reads'), measure.vars = c('Herror', 'Serror'))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## Make vector of colors for values smaller than 0 (20 colors)
-rc1 <- colorRampPalette(colors = c('firebrick1','tan1', 'lemonchiffon'), method = 'linear')
-
-## Make vector of colors for values larger than 0 (180 colors)
-rc2 <- colorRampPalette(colors = c('lemonchiffon','tan1', 'firebrick1'), method = 'linear')
-
-## Combine the two color palettes
-rampcols <- c(rc1, rc2)
-
-mypal <- colorNumeric(palette = rampcols, domain = gg$Herror)
-
-## If you want to preview the color range, run the following code
-filled.contour(d2plot_H,
-               color.palette = mypal,
-               plot.title = title(main = 'Herror',  ylab = 'S', xlab = 'H'),
-               plot.axes = {
-                   axis(1, at = seq(from=0, to = 1, length.out = nrow(d2plot_H)), labels = rownames(d2plot_H), las=2) #axis = 1 below
-                   axis(2, at = seq(from=0, to = 1, length.out = ncol(d2plot_H)), labels = colnames(d2plot_H), cex.axis=0.9, las=2) #axis = 2 left
-                   #axis(2, at =seq(from=0,to=1, length.out = 5), labels =c(4,3,2,1,0), cex.axis=0.9, las=2)
-                   
-               }
-)
-
-ggmelt = melt(gg, id.var = c('H','S'), measure.vars = c('Herror'))
-p = ggplot(ggmelt, aes(H, S)) + theme_light()
+p = ggplot(ggmelt, aes(H, S)) + theme_light() + facet_grid(reads~variable)
 p = p + geom_raster(aes(fill = value), interpolate = T)
-p = p + labs(title = 'Shannon reference values', x = 'H', y = 'S', fill = 'H relative error')
+p = p + labs(title = 'Shannon reference values', x = 'H', y = 'S', fill = 'Relative error')
 p = p + theme(plot.title = element_text(hjust = 0.5))
-p = p + scale_fill_gradient2(low = 'firebrick', mid = 'lemonchiffon', high = 'tan1', na.value = 'white')
+p = p + scale_fill_gradient2(low = 'firebrick', mid = 'yellowgreen', high = 'firebrick', na.value = 'white')
 p
+
+ggsave(file = paste0(projectName , '.', 'relativeError.svg'), plot = p, width = 10, height = 8)
+
+
+
+# La idea de usar errores relativos es que precisamente iguala las diferencias cuando se miran a una escala pequeña, por ejemplo H, 1(pedido) y 3 (obs) o con números más altos (100 S o 50S)
+
+# Calculate number of reads
+
+rr = dd
+
+
+
+gg2 = expand.grid(H = Hvalues, S = Svalues)
+gg2$ValidH = rep(NA, nrow(gg2))
+gg2$ValidS = rep(NA, nrow(gg2))
+rownames(gg2) = paste0(gg2$H, '_', gg2$S)
+
+
+for (r in sort(reads)){
+    print(r)
+    srr = rr[rr$reads == r, ]
+    # Fix a relative error to be considered acceptable: Ex: 0.10
+    for (i in c(1:nrow(srr))) {
+        print(r)
+        #print(srr[i,])
+        #print(gg2[paste0(srr$H[i], '_', srr$S[i]),])
+        if (is.na(srr$Herror[i])){
+            print(1)
+            next()
+        } else if(gg2[paste0(srr$H[i], '_', srr$S[i]), 'ValidH'] != 0){
+            print(2)
+            next()
+        } else {
+            print(3)
+            if (abs(srr$Herror[i]) < 0.1){
+                print('fff')
+                gg2[paste0(srr$H[i], '_', srr$S[i]), 'ValidH'] = r # save minimun number of reads to satisfy this criteria
+            }else if (abs(srr$Herror[i]) > 0.1){
+                print('gg')
+                gg2[paste0(srr$H[i], '_', srr$S[i]), 'ValidH'] = 0
+            }else{next()}
+        }
+        print(gg2[paste0(srr$H[i], '_', srr$S[i]), 'ValidH'])
+    }
+}
+    
+
+    ggplot(gg2, aes(x = H, y = S, fill = ValidH)) + geom_tile()
+
+
+
+
+
+
